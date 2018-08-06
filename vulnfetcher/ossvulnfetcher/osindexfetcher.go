897561667722deb9c4451e05b8350da0f293cc84
Florian Traverse <florian.traverse@rocksolead.com>
2018-08-06 13:38:45 +0200

    fix: OSSIndex failures would not output error

--- vulnfetcher/ossvulnfetcher/osindexfetcher.go
+++ vulnfetcher/ossvulnfetcher/osindexfetcher.go
@@ -134,15 +134,15 @@ func (n *OSSIndexFetcher) testBatch(pkgs []nodepackage.NodePackage) ([]vulnfetch
 	responseData, err := ioutil.ReadAll(response.Body)
 	switch {
 	case s >= 500:
-		log.Fatalf("Error 500:\n%s\nquery:\n%s", responseData, data)
-		return nil, fmt.Errorf("Error: OSSIndex is unavailable:\n%s\nclient request resulting in error: %s", responseData, data)
+		log.Printf("Error 500:\n%s\nquery:\n%s", responseData, data)
+		return nil, fmt.Errorf("OSSIndex is unavailable:\n%s\nclient request resulting in error: %s", responseData, data)
 	case s == 429:
-		log.Fatalf("Should retry:\n%s\nquery:\n%s", responseData, data)
-		return nil, fmt.Errorf("Error: OSSIndex : 'Too many requests':\n%s\nclient request resulting in error: %s", responseData, data)
+		log.Printf("Should retry:\n%s\nquery:\n%s", responseData, data)
+		return nil, fmt.Errorf("OSSIndex : 'Too many requests':\n%s\nclient request resulting in error: %s", responseData, data)
 	case s >= 400:
-		log.Fatalf("Error 40X:\n%s\nquery:\n%s", responseData, data)
+		log.Printf("Error 40X:\n%s\nquery:\n%s", responseData, data)
 		// Don't retry, it was client's fault
-		return nil, fmt.Errorf("Error: OSSIndex client error:\n%s\nclient request resulting in error: %s", responseData, data)
+		return nil, fmt.Errorf("OSSIndex client error:\n%s\nclient request resulting in error: %s", responseData, data)
 	}
 
 	if err != nil {
